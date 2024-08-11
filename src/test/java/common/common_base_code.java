aa4a6a048cbfcba9af67956b07d6ac2bc2633503
Nagaraj_kumbar <nagarajkumbar2225@gmail.com>
2024-08-11 13:10:12 +0530

    added new code

---
+++ src/test/java/common/common_base_code.java
@@ -0,0 +1,23 @@
+package common;
+
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class common_base_code {
+	
+	public static WebDriver driver=null;  //WebDriver declartion
+	
+	@BeforeSuite
+	public void laucnch_browser() {
+	 driver = new ChromeDriver();
+		
+	}
+	
+	@AfterSuite
+	public void close_browser() {
+		driver.close();
+	}
+	
+}
--- src/test/java/maven_java_framework/java_selenium_examples/selenium_examples.java
+++ src/test/java/maven_java_framework/java_selenium_examples/selenium_examples.java
@@ -1,5 +1,7 @@
 package maven_java_framework.java_selenium_examples;
 
+import static org.testng.Assert.assertEquals;
+
 import java.time.Duration;
 
 import org.openqa.selenium.By;
@@ -9,32 +11,40 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class login_example {
+import common.common_base_code;
+
+
+public class selenium_examples 	extends common_base_code {
 	@Test
 	public void login() throws InterruptedException {
-		WebDriver driver = new ChromeDriver();
-		// set implicit wait
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		driver.get("https://rahulshettyacademy.com/locatorspractice/");
-
+		//use WebDriver so i will support all kind of browser drivers if u use specific browser driver it might not support some feature 
+		//WebDriver driver = new ChromeDriver();
+		// set implicit wait (It will wait until web objects are loaded into screen)
+		//driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://rahulshettyacademy.com/locatorspractice/");  // open URL
+		//or
+		driver.navigate().to("https://rahulshettyacademy.com/locatorspractice/");
 		driver.manage().window().maximize(); // full screen
-		driver.getCurrentUrl();
-		System.out.println(driver.getCurrentUrl());
-		driver.getTitle();
-		System.out.println(driver.getTitle());
+		driver.navigate().refresh(); 
+		driver.getCurrentUrl(); 
+		System.out.println(driver.getCurrentUrl()); 
+		driver.getTitle(); //get web page title
+		System.out.println(driver.getTitle()); //print web title 
 		// Thread.sleep(5000);
 		driver.findElement(By.xpath("//input[@placeholder=\"Username\"]")).sendKeys("abc@gamil.com");
 		driver.findElement(By.cssSelector("input[placeholder=\"Password\"]")).sendKeys("rahulshettyacademy");
 		driver.findElement(By.xpath("//button[@type=\"submit\"]")).click();
+		
+		
 		// Thread.sleep(5000);
 		//WebElement loginsuccess = driver.findElement(By.xpath("//p[ text() = \"You are successfully logged in.\" ]"));
-		//System.out.println(loginsuccess.getText());
-		
+		//System.out.println(loginsuccess		.getText());
+
 		
 		
 		
 		String actualUrl=driver.getCurrentUrl();
-		String expectedUrl= "https://rahulshettyacademy.com/locatorspractice/1";
+		String expectedUrl= "https://rahulshettyacademy.com/locatorspractice/";
 		//System.out.println("actual url datatype"+actualUrl.getClass().getSimpleName());
 		//System.out.println("expected url datatype"+expectedUr.getClass().getSimpleName());
 	
@@ -48,16 +58,33 @@ public class login_example {
 			System.out.println("login failed");
 		}
 		
+		//or
+		
+		assertEquals(actualUrl, expectedUrl);
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//button[@class=\"logout-btn\"]")).click();
+		System.out.println(driver.findElement(By.xpath("//h1[text()=\"Sign in\"]")).getText());
+		System.out.println("signed  out and returned to sign in");
 		
 		
+		
+		
+
 		/*if (loginsuccess.getText().equalsIgnoreCase("You are successfully logged in.")) {
 			System.out.println("You are successfully logged in");
 		} else {
 			System.out.println("Please check login details ");
 		}
 		*/
-		driver.quit();
+			
 
+	}
+	
+	@Test
+	public void static_dropdown()
+	{
+		driver.get("");
+		
 	}
 
 }
