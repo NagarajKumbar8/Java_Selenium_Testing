3a27037c2423da3e0e20d8ae462e254d1840d60b
Nagaraj_kumbar <nagarajkumbar2225@gmail.com>
2025-07-04 14:07:18 +0530

    java pgms

--- src/test/java/maven_java_framework/java_selenium_examples/selenium_examples.java
+++ src/test/java/maven_java_framework/java_selenium_examples/selenium_examples.java
@@ -595,33 +595,7 @@ public class selenium_examples extends common_base_code {
 		System.out.println(namefield.getRect().getDimension().getWidth());
 		
 	}
-	
-	@Description (" Reverse string and check palindrome")
-	@Test
-	public void reverseString()
-	{
-	    String name = "madam";
-	    String reverse = "";
-
-	    // Iterate through the string in reverse order
-	    for (int i = name.length() - 1; i >= 0; i--)
-	    {
-	        reverse += name.charAt(i); // Correctly append characters
-	    }
-
-	    // Output the reversed string
-	    System.out.println("Reversed string: " + reverse);
-	    
-	    //Check palindrom string 
-	    if(name.equalsIgnoreCase(reverse))
-	    {
-	    	System.out.println("its palindrom");
-	    
-	    }
-	    else {
-	    	System.out.println("its not palindrom");
-	    }
-	}
+		
 	
 	@Test
 	public void searchcommon() throws InterruptedException
@@ -655,68 +629,6 @@ public class selenium_examples extends common_base_code {
 		
 	}	
 
-	@Test
-	public void prinrepatedchar() {
-
-		        String input = "my name is nagaraj";
-		        StringBuilder checked = new StringBuilder(); // To track processed characters
-
-		        // Outer loop to pick each character
-		        for (int i = 0; i < input.length(); i++) {
-		            char currentChar = input.charAt(i);
-
-		            // Skip already processed characters
-		            if (checked.toString().indexOf(currentChar) != -1) {
-		                continue;
-		            }
-
-		            // Inner loop to count occurrences of the current character
-		            int count = 0;
-		            for (int j = 0; j < input.length(); j++) {
-		                if (input.charAt(j) == currentChar) {
-		                    count++;
-		                }
-		            }
-
-		            // Mark the character as processed
-		            checked.append(currentChar);
-
-		            // Print the result
-		            System.out.println("Character '" + currentChar + "' repeated " + count + " times.");
-		        }
-		    }
-		
-
-	@Test
-	public void revwords()
-	{
-		String name="My name is nagaraj";
-		String[] split= name.split(" ");
-		String rev= " ";
-		
-		for(int i=split.length-1; i>=0; i--)
-		{
-			rev= rev + split[i] + " " ;	
-		}
-		System.out.println(rev);
-	
-	}
-	
-	@Test
-	public void RevStringbuilder()
-	{
-		 StringBuilder str = new StringBuilder("abcd abc");
-
-		    // Convert StringBuilder to String to use split()
-		    String[] split = str.toString().split(" ");
-
-		    // Print reversed string
-		    System.out.println(str.reverse());
-
-		}
-		
-		
-	
 	
 }
 
